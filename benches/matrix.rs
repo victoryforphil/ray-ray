42fbde1b134b968c229ad865e46a84ceaee47624
Alex Carter <victoryforphil@gmail.com>
2024-05-26 02:12:52 -0700

    Added benchmarks

--- benches/matrix.rs
+++ benches/matrix.rs
@@ -18,8 +18,36 @@ fn matrix_mul_4() -> Matrix<4, 4> {
     a * b
 }
 
+fn create_matrix_4() -> Matrix<4, 4> {
+    let a = Matrix::new([
+        [2.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 8.0],
+        [9.0, 8.0, 7.0, 6.0],
+        [5.0, 4.0, 3.0, 2.0],
+    ]);
+    a
+}
+
+fn transpose_matrix_4() -> Matrix<4, 4> {
+    let a = Matrix::new([
+        [2.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 8.0],
+        [9.0, 8.0, 7.0, 6.0],
+        [5.0, 4.0, 3.0, 2.0],
+    ]);
+    a.transpose()
+}
+
+fn test_matrix_det_4() -> f64 {
+    let a = Matrix::new([[1.0, 5.0], [-3.0, 2.0]]);
+    a.determinate()
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Matrix Multiply", |b| b.iter(|| matrix_mul_4()));
+    c.bench_function("Create Matrix", |b| b.iter(|| create_matrix_4()));
+    c.bench_function("Transpose Matrix", |b| b.iter(|| transpose_matrix_4()));
+    c.bench_function("Matrix Determinate", |b| b.iter(|| test_matrix_det_4()));
 }
 
 criterion_group!(benches, criterion_benchmark);
