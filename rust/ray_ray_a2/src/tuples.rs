2f7991f0c90b61f4b261c889cba62347b779e82f
Alex Carter <victoryforphil@gmail.com>
2024-12-11 03:52:50 -0800

    [Rust / a2] Added basic tuple math opps

--- rust/ray_ray_a2/src/tuples.rs
+++ rust/ray_ray_a2/src/tuples.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, Div, Mul, Neg, Sub};
 
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, PartialOrd)]
@@ -51,6 +51,32 @@ impl From<(f64, f64, f64,f64)> for Tuple{
     }
 }
 
+impl From<[f64;3]> for Tuple{
+    fn from(value: [f64;3]) -> Self {
+        Self::new_vector(value[0], value[1], value[2])
+    }
+}
+
+impl From<(f64, f64, f64)> for Tuple{
+    fn from(value: (f64, f64, f64)) -> Self {
+         let (x, y, z) = value;
+         Self::new_point(x, y, z)
+    }
+}
+
+impl Neg for Tuple {
+    type Output = Tuple;
+
+    fn neg(self) -> Self::Output {
+        Self{
+            x: 0.0 - self.x,
+            y: 0.0 - self.y,
+            z: 0.0 - self.z,
+            w: 0.0 - self.w,
+        }
+    }
+}
+
 impl Add for Tuple{
     type Output = Tuple;
 
@@ -62,6 +88,48 @@ impl Add for Tuple{
             w: self.w + rhs.w
         }
     }
+}
+
+impl Sub for Tuple{
+    type Output = Tuple;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self{
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+            w: self.w - rhs.w
+        }
+    }
+}
+
+
+impl Mul<f64> for Tuple{
+    type Output = Tuple;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self{
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+            w: self.w * rhs
+        }
+    }
+
+}
+
+impl Div<f64> for Tuple{
+    type Output = Tuple;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Self{
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+            w: self.w / rhs
+        }
+    }
+
 }
 
 #[cfg(test)]
@@ -96,6 +164,9 @@ mod test{
         assert_eq!(t_vector, [4.0, -4.0, 3.0, 0.0].into());
         assert!(t_vector.is_vector());
         assert!(!t_vector.is_point());
+
+        let t_auto: Tuple = [4.0, -4.0, 3.0].into();
+        assert_eq!(t_auto, t_vector);
     }
 
     #[test]
@@ -104,6 +175,10 @@ mod test{
         assert_eq!(t_point, [4.0, -4.0, 3.0, 1.0].into());
         assert!(t_point.is_point());
         assert!(!t_point.is_vector());
+
+
+        let t_auto: Tuple = (4.0, -4.0, 3.0).into();
+        assert_eq!(t_auto, t_point);
     }
 
     #[test]
@@ -114,5 +189,92 @@ mod test{
         let t_a = t_1 + t_2;
 
         assert_eq!(t_a, [1.0, 1.0, 6.0, 1.0].into());
+    }
+
+    #[test]
+    pub fn test_sub_points(){
+        let p1: Tuple = (3.0, 2.0, 1.0).into();
+        let p2: Tuple = (5.0, 6.0, 7.0).into();
+
+        let va = p1-p2;
+
+        assert!(va.is_vector()); // Result is a vector! 
+        assert_eq!(va, [-2.0, -4.0, -6.0].into());
+    }
+
+    #[test]
+    pub fn test_sub_point_vector(){
+        let p: Tuple = (3.0, 2.0, 1.0).into();
+        let v: Tuple = [5.0, 6.0, 7.0].into();
+
+        let pa = p-v;
+
+        assert!(pa.is_point()); // Result is a point! 
+        assert_eq!(pa, (-2.0, -4.0, -6.0).into());
+    }
+
+    #[test]
+    pub fn test_sub_vector(){
+        let v1: Tuple = [3.0, 2.0, 1.0].into();
+        let v2: Tuple = [5.0, 6.0, 7.0].into();
+
+        let va = v1-v2;
+
+        assert!(va.is_vector()); // Result is a point! 
+        assert_eq!(va, [-2.0, -4.0, -6.0].into());
+    }
+
+    #[test]
+    pub fn test_sub_zero(){
+        let v1: Tuple = [0.0, 0.0, 0.0].into();
+        let v2: Tuple = [3.0, 2.0, 1.0].into();
+
+        let va = v1-v2;
+
+        assert!(va.is_vector()); // Result is a point! 
+        assert_eq!(va, [-3.0, -2.0, -1.0].into());
+    }
+
+    #[test]
+    pub fn test_negate(){
+  
+        let v: Tuple = [3.0, 2.0, 1.0].into();
+
+        let va = -v;
+
+        assert!(va.is_vector()); // Result is a point! 
+        assert_eq!(va, [-3.0, -2.0, -1.0].into());
+    }
+
+
+    #[test]
+    pub fn test_mul_scalar(){
+  
+        let t: Tuple = [1.0, -2.0, 3.0, -4.0].into();
+
+        let ta = t * 3.5;
+
+        assert_eq!(ta, [3.5, -7.0, 10.5, -14.0].into());
+    }
+
+
+    #[test]
+    pub fn test_mul_fraction(){
+  
+        let t: Tuple = [1.0, -2.0, 3.0, -4.0].into();
+
+        let ta = t * 0.5;
+
+        assert_eq!(ta, [0.5, -1.0, 1.5, -2.0].into());
+    }
+
+    #[test]
+    pub fn test_div_scalar(){
+  
+        let t: Tuple = [1.0, -2.0, 3.0, -4.0].into();
+
+        let ta = t  / 2.0;
+
+        assert_eq!(ta, [0.5, -1.0, 1.5, -2.0].into());
     }
 }
