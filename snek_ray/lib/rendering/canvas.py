088239204dc2387ff67fbbbdf96c0106abc4d6f5
Alex Carter <victoryforphil@gmail.com>
2025-11-13 18:52:00 -0800

    PPM as str

--- snek_ray/lib/rendering/canvas.py
+++ snek_ray/lib/rendering/canvas.py
@@ -11,3 +11,29 @@ class Canvas:
 
     def pixel_at(self, x, y):
         return self.data[y][x]
+
+    def as_ppm(self):
+
+        ppm_string = "P3\n" # Start with magic number p3
+
+        # Append {width} {height}
+        ppm_string += f"{self.width} {self.height}\n"
+
+        # Add max color (255)
+        ppm_string += "255\n"
+
+        # Add each color row by row to PPM data
+        for y in range(self.height):
+            for x in range(self.width):
+                pixel = self.pixel_at(x,y) 
+                r = max(0, pixel.r)
+                g = max(0, pixel.g)
+                b = max(0, pixel.b)
+
+            
+                r = int(min(r * 255, 255))
+                g = int(min(g * 255, 255))
+                b = int(min(b * 255, 255))
+                ppm_string += f"{r} {g} {b} "
+            ppm_string += "\n" #End of image row 
+        return ppm_string
--- snek_ray/test/test_canvas.py
+++ snek_ray/test/test_canvas.py
@@ -10,3 +10,42 @@ def test_canvas_create_blank():
     for y in range(canvas.height):
         for x in range(canvas.width):
             assert canvas.data[y][x] == Color(0.0, 0.0, 0.0)
+
+def test_canvas_write_read():
+    canvas = Canvas(10, 20)
+
+    red = Color(1.0, 0., 0.)
+
+    canvas.write_pixel(2,3, red)
+
+    assert canvas.pixel_at(2,3) == Color(1., 0., 0.)
+
+def test_canvas_ppm_header():
+    canvas = Canvas(10, 20)
+
+    ppm_str = canvas.as_ppm()
+
+    lines = ppm_str.splitlines()
+
+    assert lines[0] == "P3"
+    assert lines[1] == "10 20"
+    assert lines[2] == "255"
+
+def test_canvas_ppm_color_data():
+    canvas = Canvas(5, 3)
+
+    c1 = Color(1.5, 0., 0.)
+    c2 = Color(0., 0.5, 0.)
+    c3 = Color(-0.5, 0., 1.)
+
+    canvas.write_pixel(0,0, c1)
+    canvas.write_pixel(2,1, c2)
+    canvas.write_pixel(4,2, c3)
+
+    ppm_str = canvas.as_ppm()
+
+    lines = ppm_str.splitlines()
+    
+    assert lines[3] == "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "
+    assert lines[4] == "0 0 0 0 0 0 0 127 0 0 0 0 0 0 0 " 
+    assert lines[5] == "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255 " 
